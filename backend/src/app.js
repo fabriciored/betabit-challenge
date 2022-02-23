dc667bfb77056ecf38176458c44c993a5a274ee9
fabriciored <fabriciojlsantos@gmail.com>
2022-02-23 18:28:59 -0300

    frontend: create vite react-ts app ⚙

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const controllers = require('./controllers')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.json())
 
