633f2c25f39d038bb193e8674cf9f80dddc8dcf0
fabriciored <fabriciojlsantos@gmail.com>
2022-02-23 16:52:42 -0300

    backend: create CRUD operations routes✅

--- backend/src/controllers/contactsController.js
+++ backend/src/controllers/contactsController.js
@@ -4,10 +4,47 @@ const { Contact } = require('../models');
 
 const router = Router();
 
-router.get('/', (request, response) => {
-
-
-    response.status(200).json({message: 'Ok!'})
+router.get('/', async (request, response) => {
+    const contacts = await Contact.findAll()
+    response.status(200).json(contacts)
 })
+
+router.get('/:id', async (request, response) => {
+    const contacts = await Contact.findByPk(request.params.id)
+    response.status(200).json(contacts)
+})
+
+router.post('/', async (request, response) => {
+    const {name, phone, email, image} = request.body
+    const newContact = Contact.create({name, phone, email, image})
+
+    response.status(200).json({message: 'Contact added', content: `${newContact}`})
+})
+
+router.put('/:id', async(request, response) => {
+    const {name, phone, email, image} = request.body
+
+    await Contact.update(
+        {
+            name, phone, email, image
+        },
+        {
+            where: {
+                id: request.params.id
+            }
+        }
+    )
+    response.status(200).json({message: 'Contact updated'})
+})
+
+router.delete('/:id', async(request, response) => {
+    await Contact.destroy({
+        where: {
+            id: request.params.id
+        }
+    })
+    response.status(200).json({message: 'Contact deleted', deleted: `${request.params.id}`})
+})
+
 
 module.exports = router;
